c059d005d403b574bec6bb5f2a00585a7527906c
Colacys <colacys@gmail.com>
2026-02-27 22:06:20 +0100

    chore(test): migrate from happy-dom to jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'happy-dom',
+    environment: 'jsdom',
     setupFiles: ['./src/tests/setup.ts'],
   },
   build: {
