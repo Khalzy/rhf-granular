e244435d87446334c5fe4c691dd2710cdcd0efbb
Colacys <colacys@gmail.com>
2026-02-27 21:52:01 +0100

    feat(useFormSelector): add proxy-based selective subscription

--- src/useFormSelector.ts
+++ src/useFormSelector.ts
@@ -1,7 +1,13 @@
 import { useCallback, useRef, useSyncExternalStore } from "react";
-import type { Control, FieldValues, FormState } from "react-hook-form";
-import { subscribeManager } from "./subscribe";
+import { type Control, type FieldValues, type FormState } from "react-hook-form";
+import { useStableRef } from "./hooks/useStableRef";
+import { registerSubscriber } from "./registerSubscriber";
+import { selectWithProxy } from "./selectWIthProxy";
 import type { EqualityFn, Subscriber } from "./types/manager";
+import { PathMeta } from "./types/pathMeta";
+import { getManager } from "./getManager";
+import { removeSubscriber } from "./removeSubscriber";
+import { hasWatchedKeysChanged } from "./utils/hasWatchedKeysChanged";
 import { shallow } from "./utils/shallow";
 
 /**
@@ -17,70 +23,129 @@ import { shallow } from "./utils/shallow";
  *   return base * values.seats;
  * });
  */
-export function useFormSelector<TFieldValues extends FieldValues = FieldValues, T = unknown>(
+
+export function useFormSelector<T, TFieldValues extends FieldValues = FieldValues>(
     control: Control<TFieldValues>,
     selector: (state: Partial<FormState<TFieldValues>> & { values: TFieldValues }) => T,
-    options: {
-        equalityFn: EqualityFn<T>
-    } = { equalityFn: shallow }
-): T {
-    const selectorRef = useRef(selector);
-    selectorRef.current = selector;
+    options?: {
+        equalityFn?: EqualityFn<T>,
+    }
+): T | undefined {
+    const selectorRef = useStableRef(selector);
+    const equalityRef = useStableRef(options?.equalityFn ?? shallow);
 
-    const equalityRef = useRef(options.equalityFn);
-    equalityRef.current = options.equalityFn;
-
+    const watchedMeta = useRef<Map<string, PathMeta>>(new Map());
     const watchedKeys = useRef<Set<string>>(new Set());
 
-    const lastResult = useRef<{ state: Partial<FormState<TFieldValues>>; value: T } | null>(null);
+    const draftWatchedMeta = useRef<Map<string, PathMeta>>(new Map());
+    const draftWatchedKeys = useRef<Set<string>>(new Set());
 
+    const lastResult = useRef<{ value?: T } | null>(null);
     const subscriberRef = useRef<Subscriber<T, TFieldValues> | null>(null);
 
-    const stableSelector = useCallback((state: Partial<FormState<TFieldValues>> & { values: TFieldValues }): T => selectorRef.current(state), [])
+    const lastSnapshotState = useRef<Partial<FormState<TFieldValues>> & { values: TFieldValues }>({
+        ...control._formState,
+        values: control._formValues as TFieldValues,
+    });
 
+    const lastManagerState = useRef<Partial<FormState<TFieldValues>> & { values: TFieldValues }>({
+        ...control._formState,
+        values: control._formValues as TFieldValues,
+    });
+
+    const stableSelector = useCallback(
+        (
+            state: Partial<FormState<TFieldValues>> & { values: TFieldValues }
+        ): T => selectorRef.current(state),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [])
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const stableEqualityFn: EqualityFn<T> = useCallback((a, b) => equalityRef.current(a, b), [])
 
-    const getSnapshot = useCallback((): T => {
-        const currentState = { ...control._formState, values: control._formValues as TFieldValues };
+    const getSnapshot = useCallback((): T | undefined => {
+        const rawState = {
+            ...control._formState,
+            values: control._formValues,
+            defaultValues: control._defaultValues
+        } as Partial<FormState<TFieldValues>> & { values: TFieldValues }
 
-        if (lastResult.current && Object.is(lastResult.current.state, control._formState)) {
-            return lastResult.current.value;
+        if (!subscriberRef.current) {
+            const newValue = selectWithProxy(stableSelector, rawState, draftWatchedKeys, draftWatchedMeta)
+
+            lastSnapshotState.current = structuredClone(rawState)
+
+            if (stableEqualityFn(newValue, lastResult.current?.value)) {
+                return lastResult.current?.value
+            }
+
+            lastResult.current = { value: newValue };
+
+            return newValue
         }
 
-        const newValue = selectorRef.current(currentState);
+        const shouldNotify = hasWatchedKeysChanged(
+            watchedKeys,
+            watchedMeta,
+            rawState,
+            lastSnapshotState,
+        )
 
-        if (lastResult.current && equalityRef.current(newValue, lastResult.current.value)) {
-            return lastResult.current.value;
+
+        if (!shouldNotify) {
+            return lastResult.current?.value;
         }
 
-        lastResult.current = { state: control._formState, value: newValue };
+        const newValue = selectWithProxy(stableSelector, rawState, watchedKeys, watchedMeta)
+
+        lastSnapshotState.current = structuredClone(rawState);
+
+        if (stableEqualityFn(newValue, lastResult.current?.value)) {
+            return lastResult.current?.value;
+        }
+
+        lastResult.current = { value: newValue };
+
         return newValue;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [control])
+
+    const subscribe = useCallback((onStoreChange: () => void) => {
+        if (!subscriberRef.current) {
+            const initialState = {
+                ...structuredClone(control._formState),
+                values: structuredClone(control._formValues),
+                defaultValues: structuredClone(control._defaultValues)
+            } as Partial<FormState<TFieldValues>> & { values: TFieldValues }
+
+            const initialValue = selectWithProxy(stableSelector, initialState, watchedKeys, watchedMeta);
+
+            lastManagerState.current = initialState
+
+            subscriberRef.current = {
+                callback: onStoreChange,
+                selector: stableSelector,
+                equalityFn: stableEqualityFn,
+                watchedKeys,
+                watchedMeta,
+                lastValue: initialValue,
+                lastFormState: lastManagerState,
+                type: 'value',
+                selectorSource: 'user'
+            };
+
+            registerSubscriber(control, subscriberRef)
+        } else {
+            subscriberRef.current.callback = onStoreChange;
+        }
+
+        const manager = getManager(control)
+        return () => removeSubscriber(control, manager, subscriberRef)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [control])
 
     return useSyncExternalStore(
-        (onStoreChange) => {
-            const currentState = { ...control._formState, values: control._formValues as TFieldValues };
-
-            if (!subscriberRef.current) {
-                subscriberRef.current = {
-                    callback: onStoreChange,
-                    selector: stableSelector,
-                    equalityFn: stableEqualityFn,
-                    watchedKeys,
-                    lastValue: stableSelector(currentState),
-                    lastFormState: control._formState,
-                    type: 'value',
-                };
-            } else {
-                subscriberRef.current.callback = onStoreChange;
-            }
-
-            const unsubscribe = subscribeManager(control, subscriberRef.current)
-
-            return () => {
-                unsubscribe()
-            }
-        },
+        subscribe,
         getSnapshot,
         getSnapshot
     );
