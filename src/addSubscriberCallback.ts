18fab9e18691ad22ef23a4ddd8300acb7fdbd9a6
Colacys <colacys@gmail.com>
2026-02-27 21:45:46 +0100

    chore(subscription): update types for subscriber callbacks

--- src/addSubscriberCallback.ts
+++ src/addSubscriberCallback.ts
@@ -2,9 +2,11 @@ import { FieldValues } from "react-hook-form";
 import { Subscriber, SubscriptionManager } from "./types/manager";
 import { RefObject } from "react";
 
-export function addSubscriberCallback<T, TFieldValues extends FieldValues>(
-    manager: SubscriptionManager,
-    subscriberRef: RefObject<Subscriber<T, TFieldValues>>
+export function addSubscriberCallback<T, TFieldValues extends FieldValues = FieldValues>(
+    manager: SubscriptionManager<T, TFieldValues>,
+    subscriberRef: RefObject<Subscriber<T, TFieldValues> | null>
 ) {
+    if (!subscriberRef.current) return
+
     manager.subscribers.set(subscriberRef.current.callback, subscriberRef)
 }
