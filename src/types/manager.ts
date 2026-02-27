bdf9c1c88857e41899ab05641aae8435db5d43cd
Colacys <colacys@gmail.com>
2026-02-27 21:53:19 +0100

    chore(types): add watched keys, meta and selector source to subscriber interface

--- src/types/manager.ts
+++ src/types/manager.ts
@@ -1,23 +1,25 @@
 import { RefObject } from "react";
 import type { FieldValues, FormState } from "react-hook-form";
+import { PathMeta } from "./pathMeta";
 
 export type EqualityFn<T> = (a?: T, b?: T | null) => boolean;
 
 export type SubscriberCallback<TFieldValues extends FieldValues = FieldValues> = ((state?: Partial<FormState<TFieldValues>> & { values: TFieldValues }) => void)
 
-export interface Subscriber<T = unknown, TFieldValues extends FieldValues = FieldValues> {
+export interface Subscriber<T, TFieldValues extends FieldValues = FieldValues> {
     callback: SubscriberCallback<TFieldValues>
-    selector?: (state: Partial<FormState<TFieldValues>> & { values: TFieldValues }) => T
+    selector: (state: Partial<FormState<TFieldValues>> & { values: TFieldValues }) => T | undefined
     equalityFn?: EqualityFn<T>
     watchedKeys: RefObject<Set<string>>;
+    watchedMeta: RefObject<Map<string, PathMeta>>;
     lastValue?: T | null;
-    lastFormState: Partial<FormState<TFieldValues>>
-    type: 'effect' | 'value'
+    lastFormState: RefObject<Partial<FormState<TFieldValues>> & { values: TFieldValues }>
+    type: 'effect' | 'value';
+    selectorSource: 'user' | 'default'
 }
 
-export interface SubscriptionManager {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    subscribers: Map<() => void, Subscriber<any, any>>
+export interface SubscriptionManager<T, TFieldValues extends FieldValues = FieldValues> {
+    subscribers: Map<() => void, RefObject<Subscriber<T, TFieldValues> | null>>
     unsubscribe: () => void;
     prev?: FieldValues
 }
